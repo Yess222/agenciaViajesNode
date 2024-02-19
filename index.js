2555954aa9c62997d074503bdbd44289e1c7104d
Yess222 <jesus.zavaleta10@hotmail.com>
2024-02-19 12:05:01 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import router from './routes/index.js';
+import db from './config/db.js';
+dotenv.config();
+
+
+
+const app = express();
+
+// Conectar la base de datos
+db.authenticate()
+    .then( () => console.log('Base de datos conectadad'))
+    .catch( error => console.log(error));
+// Definir puerto
+const port  = process.env.PORT || 4000;
+
+// Habilitar PUG
+app.set('view engine', 'pug');
+
+// Obtener el año actual
+app.use( (req, res, next) =>{
+    const year = new Date();
+    res.locals.ActualYear = year.getFullYear();
+    res.locals.nombresitio = "Agencia de Viajes";
+    return next(); 
+}); 
+
+// Agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
+// Agregar router
+app.use('/', router);
+
+app.listen(port, () =>{
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+})
+
